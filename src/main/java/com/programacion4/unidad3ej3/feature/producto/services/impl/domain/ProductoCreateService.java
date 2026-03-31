40ab329541fc9914fc9d4c66b5122126e94b0402
TobiasPagniez <tobiaspagniez1@gmail.com>
2026-03-31 08:59:55 -0300

    US01

--- src/main/java/com/programacion4/unidad3ej3/feature/producto/services/impl/domain/ProductoCreateService.java
+++ src/main/java/com/programacion4/unidad3ej3/feature/producto/services/impl/domain/ProductoCreateService.java
@@ -1,15 +1,16 @@
 package com.programacion4.unidad3ej3.feature.producto.services.impl.domain;
 
-import com.programacion4.unidad3ej3.config.exceptions.BadRequestException;
 import org.springframework.stereotype.Service;
 
-import com.programacion4.unidad3ej3.feature.producto.services.interfaces.domain.IProductoCreateService;
+import com.programacion4.unidad3ej3.config.exceptions.BadRequestException;
 import com.programacion4.unidad3ej3.feature.producto.dtos.request.ProductoCreateRequestDto;
 import com.programacion4.unidad3ej3.feature.producto.dtos.response.ProductoResponseDto;
+import com.programacion4.unidad3ej3.feature.producto.mappers.ProductoMapper;
 import com.programacion4.unidad3ej3.feature.producto.models.Producto;
 import com.programacion4.unidad3ej3.feature.producto.repositories.IProductoRepository;
-import com.programacion4.unidad3ej3.feature.producto.mappers.ProductoMapper;
 import com.programacion4.unidad3ej3.feature.producto.services.interfaces.commons.IProductoExistByNameService;
+import com.programacion4.unidad3ej3.feature.producto.services.interfaces.commons.iProductoFormateadorDeTexto;
+import com.programacion4.unidad3ej3.feature.producto.services.interfaces.domain.IProductoCreateService;
 
 import lombok.AllArgsConstructor;
 
@@ -22,8 +23,13 @@ public class ProductoCreateService implements IProductoCreateService {
 
     private final IProductoRepository productoRepository;
 
+    private final iProductoFormateadorDeTexto productoFormateadorDeTexto; 
+
     @Override
     public ProductoResponseDto create(ProductoCreateRequestDto dto) {
+
+        dto.setNombre(productoFormateadorDeTexto.CapitalizarTexto(dto.getNombre()));
+        dto.setDescripcion(productoFormateadorDeTexto.CapitalizarTexto(dto.getDescripcion()));
 
         if (productoExistByNameService.existByName(dto.getNombre())) {
             throw new BadRequestException("El nombre del producto ya existe");
---
+++ src/main/java/com/programacion4/unidad3ej3/feature/producto/services/interfaces/commons/iProductoFormateadorDeTexto.java
@@ -0,0 +1,5 @@
+package com.programacion4.unidad3ej3.feature.producto.services.interfaces.commons;
+
+public interface iProductoFormateadorDeTexto {
+    String CapitalizarTexto (String texto);
+}
